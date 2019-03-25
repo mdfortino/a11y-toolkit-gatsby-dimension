477371e9f6a8145b39300f34426b63f1e9ed9795
“MARIA <mdfortino@gmail.com>
2019-03-24 23:49:54 -0400

    add to perceivable page, begin operable page

--- src/pages/operable.js
+++ src/pages/operable.js
@@ -1,24 +1,44 @@
 import React from 'react'
 import { Link } from 'gatsby'
-
+import RTRFORM from '../images/rtrform.jpg'
+import RTRBadCode from '../images/rtrbadcode.jpg'
+import RTRGoodCode from '../images/rtrgoodcode.jpg'
 import Layout from '../components/layout'
 
 const Operable = () => (
     <Layout>
       <h1>Operable</h1>
-      <p>brief description</p>
-    <ul> 
-      <li> Text Alternatives </li> 
-      <li> Time-based Media: add captions  </li>
-      <li> Identify Purpose</li> 
-    </ul>
-      <p> 
-        <Link to="/">Home</Link>
-        </p> 
-        <p> 
-      <Link to="/toolkit">Toolkit</Link>
+    <p>Users must be able to operate the interface (the interface cannot require interaction that a user cannot perform).</p>
+    <h2>Keyboard Accessibility</h2> 
+ <p> Make sure page headings and labels for form and interactive controls are informative. 
+</p>
+<img src={RTRFORM}
+     alt="A screenshot of Rent the Runway's size and rental period forms, with red boxes around size and rental period images"
+     title="Rent the Runway Forms"></img>
+     <h3>Bad Code: </h3>
+     <img src= {RTRBadCode}alt = "A code example of bad code in hich a form is not labeled properly and therefore not accessible by keyboard"></img>
+     <h3> Good Code: </h3>
+     <img src= {RTRGoodCode}alt = "A code example of good code in hich tab index is set to zero and aria-label is included"></img>
+     <h3> How to test: </h3>
+     <ul> 
+     <li> Identify all functionality on the content.</li>
+     <li> Check that all functionality can be accessed using only the keyboard or keyboard interface.</li> 
+ </ul> 
+  <h3> Other ways to make your website operable </h3>
+  <ul> 
+    <li> Time-adjustable. Add captions to provide a way for people who are deaf or otherwise have trouble hearing the dialogue in audio visual material to be able to view the material.  </li>
+    <li> Interaction Methods. Add interaction methods o that users are able to find, navigate through and interact with web content.
+</li> 
+</ul> 
+<p><a href = "https://webaim.org/standards/wcag/checklist"> WCAG 2 Checklist</a></p> 
+  <p> 
+    <Link to="/toolkit">Toolkit</Link>
+    </p> 
+    <p> 
+      <Link to="/">Home</Link>
       </p> 
-    </Layout>
-  )
+  </Layout>
+)
+  
 
 export default Operable;
--- src/pages/perceivable.js
+++ src/pages/perceivable.js
@@ -10,6 +10,7 @@ const Perceivable = () => (
     <h1>Perceivable</h1>
     <p>Users must be able to perceive the information being presented (it can't be invisible to all of their senses).</p>
     <h2>Text Alternatives for Image </h2> 
+    <p> Include appropriate, equivalent alternative text to all images, form image buttons, and image map hot spots.</p>
     <img src={Dino}
      alt="A red Tyrannosaurus Rex: A two legged dinosaur standing upright like a human, with small arms, and a large head with lots of sharp teeth."
      title="The Mozilla red dinosaur"></img>
@@ -25,16 +26,18 @@ const Perceivable = () => (
  </ul> 
   <h3> Other ways to make your website perceivable </h3>
   <ul> 
-    <li> Time-based Media: add captions. The objective of this technique is to provide a way for people who are deaf or otherwise have trouble hearing the dialogue in audio visual material to be able to view the material.  </li>
-    <li> Identify Purpose: use ARIA landmarks to identify the regions of the page.
+    <li> Time-based Media: Add captions to provide a way for people who are deaf or otherwise have trouble hearing the dialogue in audio visual material to be able to view the material. <a href="https://www.w3.org/TR/WCAG21/#captions-prerecorded">(WCAG GL 1.2.2) </a></li>
+    <li> Identify Purpose. Use ARIA landmarks to identify the regions of the page. <a href ="https://www.w3.org/TR/WCAG21/#identify-purpose">(WCAG GL 1.3.6)</a>
 </li> 
 </ul> 
+  <p><a href = "https://webaim.org/standards/wcag/checklist"> WCAG 2 Checklist</a></p> 
+  <p> 
+    <Link to="/toolkit">Toolkit</Link>
+    </p> 
     <p> 
       <Link to="/">Home</Link>
       </p> 
-      <p> 
-    <Link to="/toolkit">Toolkit</Link>
-    </p> 
+   
   </Layout>
 )
 
